313c07c43c8d6a8012eb45f088269b3419c1278a
dirj <dmitryantonov@hotmail.com>
2021-03-28 14:55:28 +0300

    add fifth phrase

--- mew.js
+++ mew.js
@@ -2,8 +2,9 @@ const phrase1 = 'you are shining my sun today';
 const phrase2 = 'you have a beautiful day darling';
 const phrase3 = 'eat a fruit and get rest';
 const phrase4 = 'rise and shine'; 
+const phrase5 = 'you can be whoever you want from now on';
 
-const phrases = [phrase1, phrase2, phrase3, phrase4];
+const phrases = [phrase1, phrase2, phrase3, phrase4, phrase5];
 
 //generating a random phrase from our array - phrases, then loging it 
 
