6a35d6423a8c8b5643cff69cb2fda6f252702317
dirj <dmitryantonov@hotmail.com>
2021-03-28 14:53:28 +0300

    generate a random phrase to cheer you up

---
+++ mew.js
@@ -0,0 +1,15 @@
+const phrase1 = 'you are shining my sun today';
+const phrase2 = 'you have a beautiful day darling';
+const phrase3 = 'eat a fruit and get rest';
+const phrase4 = 'rise and shine'; 
+
+const phrases = [phrase1, phrase2, phrase3, phrase4];
+
+//generating a random phrase from our array - phrases, then loging it 
+
+const phraseGenerator = array => {
+    const randomPhrase = Math.floor(Math.random() * array.length);
+    console.log(phrases[randomPhrase]);
+}
+
+phraseGenerator(phrases);
